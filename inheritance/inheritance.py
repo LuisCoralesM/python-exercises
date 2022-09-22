87593acf268838c9e44dee12a1cc05d85f02bcac
Luis Corales <lcorales@stackbuilders.com>
2022-09-22 16:42:13 -0500

    Inheritance examples

---
+++ inheritance/inheritance.py
@@ -0,0 +1,10 @@
+class Person(object):
+  def __init__(self, name, id):
+    self.name = name
+    self.id = id
+
+  def Display(self):
+    print(self.name, self.id)
+
+obj1 = Person("Luis", 1)
+obj1.Display()
---
+++ inheritance/multiple.py
@@ -0,0 +1,27 @@
+class Person(object):
+  def __init__(self, id, name):
+    self.id = id
+    self.name = name
+
+  def Display(self):
+    print(self.id, self.name)
+
+class Human(object):
+  def __init__(self, bloodType):
+    self.bloodType = bloodType
+
+  def Display(self):
+    print(self.bloodType)
+
+# Multiple inheritance
+class Civilian(Person, Human):
+  def __init__(self, id, name, bloodType):
+    Person.__init__(self, id, name)
+    Human.__init__(self, bloodType)
+    print("Civilian")
+  
+  def printStr(self):
+    print(self.name, self.id, self.bloodType)
+
+obj = Civilian(1, "Luis", "O")
+obj.printStr()
